7bb8a6aabc2e9382cc881a2b4dfd1e508471d243
clément <clement.vazeille@gmail.com>
2023-12-07 02:25:51 +0100

    Le site est devenu un énorme banger

--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -6,139 +6,415 @@ import { Component } from '@angular/core';
   <section class="hero is-warning is-bold is-fullheight">
   <div class="hero-body">
 
+  <div class="columns">
+    
+  <div class="column is-3">
 
+  <div class="card" >
+  <div class="card-image" >
+    <figure class="image">
+      <img src="assets/img/Arthur.jpg">
+    </figure>
+  </div>
+  <div class="card-content">
 
-      <div class="tile is-ancestor is-vertical">
-
-      <div class="tile">
-
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-primary">
-          <p class="title">Prez</p>
-          <p class="subtitle">Arthur</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/arthur.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-danger">
-          <p class="title">Vice Prez</p>
-          <p class="subtitle">Benjamin</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/diego.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-link">
-          <p class="title">Vice Prez</p>
-          <p class="subtitle">Samuel</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/samu.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-light">
-          <p class="title">Trez</p>
-          <p class="subtitle">Maël</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/mael.png">
-          </figure>
-        </article>
-      </div>
-      </div>
-
-      <div class="tile">
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-link">
-          <p class="title">Trez</p>
-          <p class="subtitle">Louis</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/louis.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-light">
-          <p class="title">SecGen</p>
-          <p class="subtitle">Théo</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/theo.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-danger">
-          <p class="title">Respo DSI</p>
-          <p class="subtitle">Clément</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/clem.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-primary">
-          <p class="title">Respo Event</p>
-          <p class="subtitle">Louis</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/Louis-Event.png">
-          </figure>
-        </article>
-      </div>
-      </div>
-
-      <div class="tile">
-
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-primary">
-          <p class="title">Sec écolo</p>
-          <p class="subtitle">Abdelhamid</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/abdel.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-link">
-          <p class="title">Respo COM</p>
-          <p class="subtitle">Aurélien</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/aure.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-light">
-          <p class="title">Respo film</p>
-          <p class="subtitle">Diégo</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/benji.png">
-          </figure>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-dark">
-          <p class="title">Respo Sensi</p>
-          <p class="subtitle">Timothée</p>
-          <figure class="image is-4by3">
-            <img src="assets/img/timot.png">
-          </figure>
-        </article>
-      </div>
-      </div>
-
+    <div class="content">
+    <h1 class="title is-2">Arthur</h1>
+    <h2 class="subtitle is-5">Pres</h2>
     </div>
-    
-    
+  </div>
+  </div>
 
+  <section class="hero is-small">
+    <div class="hero-body">
+    </div>
+  </section>
 
-    
+  <div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+  <figure class="image">
+    <img src="assets/img/Théo.jpg" height="150" width="100">
+  </figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Théo</h1>
+    <h2 class="subtitle is-5">Sec Gen</h2>
+  </div>
+
+</div>
+</div>
+
+<section class="hero is-small">
+    <div class="hero-body">
+    </div>
+  </section>
+
+  <div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+  <figure class="image">
+    <img src="assets/img/Louis.jpg" height="150" width="100">
+  </figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Louis</h1>
+    <h2 class="subtitle is-5">Respo Event</h2>
+  </div>
+
+</div>
+</div>
+<section class="hero is-small">
+    <div class="hero-body">
+    </div>
+  </section>
+
+  <div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+  <figure class="image">
+    <img src="assets/img/Aurelien.jpg" height="150" width="100">
+  </figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Aurlien</h1>
+    <h2 class="subtitle is-5">Respo Com</h2>
+  </div>
+
+</div>
+</div>
+<section class="hero is-small">
+    <div class="hero-body">
+    </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+  <div class="card-image" height="200" width="200">
+    <figure class="image">
+      <img src="assets/img/Santiago.jpg" height="150" width="100">
+    </figure>
+  </div>
+  <div class="card-content">
+
+    <div class="content">
+    <h1 class="title is-2">Santiago</h1>
+    <h2 class="subtitle is-5">Respo Danse</h2>
+    </div>
+  </div>
+</div>
+
+</div>
+<div class="column is-3">
+
+<div class="card" >
+<div class="card-image" >
+  <figure class="image">
+    <img src="assets/img/Benjamin.jpg">
+  </figure>
+</div>
+<div class="card-content">
+
+  <div class="content">
+  <h1 class="title is-2">Benjamin</h1>
+  <h2 class="subtitle is-5">Vice Pres</h2>
+  </div>
+</div>
+</div>
+
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Kristen.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Kristen</h1>
+    <h2 class="subtitle is-5">CoSec écolo</h2>
+</div>
+
+</div>
+</div>
+
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Thibault.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Thibault</h1>
+    <h2 class="subtitle is-5">Pole event, Respo chorée</h2>
+</div>
+
+</div>
+</div>
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Samouel.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Samuel</h1>
+    <h2 class="subtitle is-5">Pole event</h2>
+</div>
+
+</div>
+</div>
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+  <figure class="image">
+    <img src="assets/img/Jeremy.jpg" height="150" width="100">
+  </figure>
+</div>
+<div class="card-content">
+
+  <div class="content">
+  <h1 class="title is-2">Jérémy</h1>
+    <h2 class="subtitle is-5">Pole event</h2>
+  </div>
+</div>
+</div>
+
+</div>
+<div class="column is-3">
+
+<div class="card" >
+<div class="card-image" >
+  <figure class="image">
+    <img src="assets/img/Samuel.jpg">
+  </figure>
+</div>
+<div class="card-content">
+
+  <div class="content">
+  <h1 class="title is-2">Samuel</h1>
+  <h2 class="subtitle is-5">Vice Pres</h2>
+  </div>
+</div>
+</div>
+
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Paulina.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Paulina</h1>
+    <h2 class="subtitle is-5">CoSec écolo</h2>
+</div>
+
+</div>
+</div>
+
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Diego.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Diego</h1>
+    <h2 class="subtitle is-5">Respo Film</h2>
+</div>
+
+</div>
+</div>
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Timothé.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Timothée</h1>
+    <h2 class="subtitle is-5">Respo Sensi</h2>
+</div>
+
+</div>
+</div>
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+  <figure class="image">
+    <img src="assets/img/Yamato.jpg" height="150" width="100">
+  </figure>
+</div>
+<div class="card-content">
+
+  <div class="content">
+  <h1 class="title is-2">Yamato</h1>
+    <h2 class="subtitle is-5">Pole event</h2>
+  </div>
+</div>
+</div>
+
+</div>
+<div class="column is-3">
+
+<div class="card" >
+<div class="card-image" >
+  <figure class="image">
+    <img src="assets/img/Louis_trez.jpg">
+  </figure>
+</div>
+<div class="card-content">
+
+  <div class="content">
+  <h1 class="title is-2">Louis</h1>
+  <h2 class="subtitle is-5">Trésorier</h2>
+  </div>
+</div>
+</div>
+
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Clément.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Clément</h1>
+    <h2 class="subtitle is-5">Respo DSI</h2>
+</div>
+
+</div>
+</div>
+
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Baptiste.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Baptiste</h1>
+    <h2 class="subtitle is-5">Respo Paris</h2>
+</div>
+
+</div>
+</div>
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+<figure class="image">
+  <img src="assets/img/Abdelhamid.jpg" height="150" width="100">
+</figure>
+</div>
+<div class="card-content">
+
+<div class="content">
+<h1 class="title is-2">Abdelhamid</h1>
+    <h2 class="subtitle is-5">Pole écolo</h2>
+</div>
+
+</div>
+</div>
+<section class="hero is-small">
+  <div class="hero-body">
+  </div>
+</section>
+
+<div class="card" radius = 100.25rem>
+<div class="card-image" height="200" width="200">
+  <figure class="image">
+    <img src="assets/img/Yannis.jpg" height="150" width="100">
+  </figure>
+</div>
+<div class="card-content">
+
+  <div class="content">
+  <h1 class="title is-2">Yanis</h1>
+    <h2 class="subtitle is-5">Pole musique</h2>
+  </div>
+</div>
+</div>
+
+</div>
+
+</div>
+
   </div>
   </section>
   `,
-  styles: [
+  styles: [`
+  .card {
+    background-color: hsl(0, 0%, 91%);
+}
+`
   ]
 })
 export class ListeComponent {
